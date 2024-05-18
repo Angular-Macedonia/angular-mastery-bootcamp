e393206e180430a1cf2d9e1243b021f3c41adab5
angel.petrushevski <angel.petrushevski@sorsix.com>
2024-05-18 20:43:58 +0800

    example 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface Person {
+  name?: string
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-mastery-bootcamp';
+  title: string | undefined | null = null;
+  name = 'angel';
+  person: Person = {
+  }
+  isClicked: boolean = false;
+
+
+   getTitle() {
+    return this.title;
+  }
+
+  getPersonName() {
+    // if(this.person?.name) {
+    //   return this.person.name
+    // }
+    // else return "no name";
+    return this.person.name ?? "no name";
+  }
+
+  //
+  onClick(event: unknown) {
+    console.log(event as PointerEvent, 'clicked');
+    // this.isClicked = true
+    // asodkoask d
+    //$distintEvent()
+  }
 }
